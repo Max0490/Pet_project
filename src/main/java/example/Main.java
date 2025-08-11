b656d7423587619acbf4b7c367ef954d30080c12
USER <seagate90@mail.ru>
2025-08-11 17:16:28 +0400

    add gradle

---
+++ src/main/java/example/Main.java
@@ -0,0 +1,9 @@
+package example;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Приложение запущено!");
+        // Здесь будет инициализация БД и Kafka
+    }
+}
--- src/main/java/example/kafka.java
+++ src/main/java/example/kafka.java
@@ -1,2 +1,4 @@
+package example;
+
 public class kafka {
 }
